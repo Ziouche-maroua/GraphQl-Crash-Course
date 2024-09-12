686b2c3ee33b14f9b52a97016e0e239bce082486
Maroua Ziouche <maroua.ziouche@etu.usthb.dz>
2024-09-12 18:11:08 +0100

    apollo client setup

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,14 +4,22 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import GridBackground from "./components/ui/GridBackground.jsx";
-//import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/',
+  cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
+  credentials: "include", // This tells Apollo Client to send cookies along with every request to the server
+});
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<GridBackground>
+      <ApolloProvider client={client}>
 				<App />
+      </ApolloProvider>
 			</GridBackground>
 		</BrowserRouter>
 	</React.StrictMode>
