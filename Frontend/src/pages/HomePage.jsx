4973b16718903893f9778520bf2e38c8026ce8f3
Maroua Ziouche <maroua.ziouche@etu.usthb.dz>
2024-09-14 14:28:34 +0100

    Clearing the cache

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -31,15 +31,14 @@ const HomePage = () => {
 		],
 	};
 
-	const [logout, { loading }] = useMutation(LOGOUT , {
+	const [logout, { loading,client }] = useMutation(LOGOUT , {
 		refetchQueries: ["GetAuthenticatedUser"],
 	});
 
 	const handleLogout = async () => {
 		try {
 			await logout();
-			// Clear the Apollo Client cache FROM THE DOCS
-			// https://www.apollographql.com/docs/react/caching/advanced-topics/#:~:text=Resetting%20the%20cache,any%20of%20your%20active%20queries
+			client.resetStore();
 			
 		} catch (error) {
 			console.error("Error logging out:", error);
